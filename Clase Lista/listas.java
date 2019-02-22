447d9433c67d4f1c3e6a6f2562c6699af2f9396a
Alejandro Ramírez Escalona <37369697+AlejandroRam@users.noreply.github.com>
2019-02-22 21:30:19 +0100

    Update listas.java

--- Clase Lista/listas.java
+++ Clase Lista/listas.java
@@ -2,22 +2,30 @@ package listas;
 
 public class listas {
 
+	/*
+	 * Elementos de la clase lista Primero un array de objetos Segundo tamaño del
+	 * array tercero numero real de elementos deel array
+	 */
 	protected Object[] contenedor;
 	protected int num_elementos;
 	protected int elementos_reales = 0;
-
+	protected int elemento_fin ;
+	/* Constructor vacío */
 	public listas() {
 
 	}
 
+	/* Constructor paramatetro tamaño */
 	public listas(int tam) {
 		contenedor = new Object[tam];
 		this.num_elementos = tam;
+		elemento_fin = tam-1;
 	}
 
 	public boolean insertar(Object objeto) throws Exception {
 		boolean exito = false;
-		if (contenedor.length - 1 == (elementos_reales)) {
+
+		 if (contenedor.length  == (elementos_reales)) {
 			throw new Exception("Fallo, lista completa");
 		} else {
 			contenedor[elementos_reales] = objeto;
@@ -30,16 +38,21 @@ public class listas {
 
 	public boolean insertar(Object objeto, int n) throws Exception {
 		boolean exito = false;
-
+		n = n-1;
 		if (n > elementos_reales) {
 			throw new Exception("posicion no disponible");
-		} else if (elementos_reales == (num_elementos - 1)) {
-			throw new Exception("fallo, lista completa");
+		
 		} else {
+			
+			if(contenedor[n]!= null)
+			{
+				contenedor[n] = objeto;
+				exito = true;
+			}else {
 			contenedor[elementos_reales] = objeto;
 			elementos_reales++;
 			exito = true;
-
+			}
 		}
 
 		return exito;
@@ -75,15 +88,14 @@ public class listas {
 		return exito;
 	}
 
-	public Object devolver()
-	{
+	public Object devolver() {
 		return contenedor[elementos_reales];
 	}
-	
+
 	/*
-	public Object devolver(int n)throws Exception
-	{
-		
-	}*/
-	
+	 * public Object devolver(int n)throws Exception {
+	 * 
+	 * }
+	 */
+
 }
